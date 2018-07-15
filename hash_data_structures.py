bb8901af005b520deafcc2e69d57754a09597d12
conradperes <conrad.marques.peres@gmail.com>
2018-07-15 20:34:15 -0300

    Add files via upload

--- hash_data_structures.py
+++ hash_data_structures.py
@@ -4,12 +4,11 @@ from utils import *
 SECURE_HASH_FUNCTIONS = ['sha1', 'sha224', 'sha256', 'sha384', 'sha512']
 
 class HashLeaf(object):
-    """Attach two pieces of string data and store the hash of the concatenated
-    strings
+    """Anexar dois pedaços de string e armazenar o hash de strings concatenadas
     """
     def __init__(self, left, right, hash_function):
         assert isinstance(hash_function, str), (
-            "Hash function is not of type `String`")
+            "Função Hash não é do tipo `String`")
         self._hash_function = hash_function
         self._left = left
         self._right = right
@@ -17,45 +16,46 @@ class HashLeaf(object):
         self._height = 1
         
     def _evaluate(self):
-        """Ensure data is in the form of a string"""
+        """Assegure mandar o dado do tipo string"""
         assert isinstance(self._left, str), "Data is not of type `String`"
         assert isinstance(self._right, str), "Data is not of type `String`"
         return hash_data(self._left + self._right, self._hash_function)
 
     @property
     def data(self):
-        """str: Allow the user to query the hashed data stored in the
-        HashLeaf
+        """str: Permite ao usuário pesquisar o dado armazenado em hash
+        no nó folha=HashLeaf
         """
         return self._data
 
     @property
     def height(self):
-        """int: Allow the user to query the height stored in the HashLeaf"""
+        """int: Permite ao usuário pesquisar a altura do dado armazenado em hash
+        no nó folha=HashLeaf"""
         return self._height
 
 class HashNode(HashLeaf):
-    """Attach two HashLeaf structures and store the hash of their concatenated
+    """Anexa duas estruturas de HashLeaf e armazena o hash do seu concatenamento
     data
     """
     def __init__(self, left, right, hash_function):
         super().__init__(left, right, hash_function)
         assert left._hash_function == hash_function, (
-            "Hash functions incompatible")
+            "Funções Hash incompatíveis")
         assert right._hash_function == hash_function, (
-            "Hash functions incompatible")
+            "Funções Hash incompatíveis")
         self._height = self._left.height + 1
 
     def _evaluate(self):
-        """Ensure data is in the form of a HashLeaf data structures and has
-        the correct height. Separate method from `HashLeaf` as there are
-        different requirements
+        """Assegure que o dado esta na forma da estrutura de dados HashLeaf e tem
+        a altura correta. Separa método de `HashLeaf` como esta em diferentes 
+        requerimentos
         """
         assert isinstance(self._left, HashLeaf), (
-            "Data is not of type `HashLeaf`")
+            "Dado não é do tipo `HashLeaf`")
         assert isinstance(self._right, HashLeaf), (
-            "Data is not of type `HashLeaf`")
+            "Dado não é do tipo `HashLeaf`")
         assert self._left.height == self._right.height, (
-            "Left and right branch not balanced")
+            "Ramo/nó da esquerda ou direita não esta balanceado")
         return hash_data(self._left.data + self._right.data,
             self._hash_function)
